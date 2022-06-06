f70a412512e1562e5fafb30b06781b8f425e4305
Alec German <kimarimt@protonmail.com>
2022-06-06 19:57:14 -0400

    used pandas to retrieve states from 50_state.csv

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import pandas as pd
+from dataclasses import dataclass
+
+
+@dataclass
+class State:
+    name: str
+    x_cor: int
+    y_cor: int
+
+
+def get_states():
+    df = pd.read_csv('data/50_states.csv')
+    states = []
+
+    for entry in df.values:
+        state = State(
+            name=entry[0],
+            x_cor=entry[1],
+            y_cor=entry[2]
+        )
+        states.append(state)
+
+    return states
+
+
 def main():
-    print('Hello, World!')
+    print(get_states())
 
 
 if __name__ == '__main__':
